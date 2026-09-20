bae4e6e068f031e233293731979e5e95c88e2726
shote2Dserv <jdjdkd32kfl@gmail.com>
2026-09-20 16:04:54 +0300

    terver.js

--- server.js
+++ server.js
@@ -125,8 +125,9 @@ setInterval(() => {
     }
 }, 30000);
 
+
 const http = require('http');
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('OK');
-}).listen(PORT + 1);
+}).listen(PORT);
